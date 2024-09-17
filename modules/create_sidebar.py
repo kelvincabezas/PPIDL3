e38da1379b0b38598c4eb487e1f951ed979010c6
kelvincabezas <ruben.cabezas@outlook.com>
2024-09-16 22:18:17 -0500

    subiendo

--- modules/create_sidebar.py
+++ modules/create_sidebar.py
@@ -16,3 +16,18 @@ def create_sidebar():
         unsafe_allow_html=True
     )
 
+    # Crear el menú de opciones en el sidebar con option_menu
+    with st.sidebar:
+        selected = option_menu("Menú", ["Inicio", "Datos", "EDA", "Modelo Predictivo"],
+            icons=["house", "database", "bar-chart-line", "airplane"],
+            menu_icon="cast", default_index=0, orientation="vertical")
+
+    # Llama a la función de la página correspondiente en función de la selección
+    if selected == "Inicio":
+        inicio.display()
+    elif selected == "Datos":
+        datos.display()
+    elif selected == "EDA":
+        eda.display()
+    elif selected == "Modelo Predictivo":
+        aviones.display()
