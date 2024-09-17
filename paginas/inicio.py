dd323ee8b24e6b1f836b2eb2974532fbbf753862
kelvincabezas <ruben.cabezas@outlook.com>
2024-09-16 22:47:30 -0500

    ssssaaaaa

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -8,12 +8,13 @@ def display():
     st.markdown("<h1 style='text-align: center;'>Desarrollo de Modelos Predictivos y Estrategias de Implementación para Optimizar la Productividad y Rentabilidad de los Pescadores Artesanales en la caleta COISHCO - 2024</h1>", unsafe_allow_html=True)
     st.markdown("<br><br>", unsafe_allow_html=True)
     
-    col1, col2, col3 = st.columns([5,0.5,2])
-    with col1:
-        st.markdown("""
-            <div style='text-align: justify;'>
-                <h3>Proyecto final para Hack a Boss</h3>
-                <p><strong style='font-size: 18px;'>Este proyecto constituye el punto culminante de nuestra formación en el Bootcamp de Data Science e Inteligencia Artificial en Hack a Boss. En nuestro tercer y último proyecto, hemos decidido abordar un desafío crítico y de alta aplicabilidad en el sector aeronáutico: la optimización de la puntualidad y la gestión de retrasos en vuelos operados desde y hacia aeropuertos en Estados Unidos. A través de un exhaustivo análisis exploratorio de datos, este estudio ofrece perspectivas sobre causas, desempeños y factores influyentes en la puntualidad aérea, proporcionando un modelo predictivo en cuanto a la puntualidad de las aerolíneas para mejorar la satisfacción de los pasajeros.</strong></p>
-            </div>
-        """, unsafe_allow_html=True)
+    st.markdown("""
+        <div style='text-align: justify;'>
+            <h4 style='text-align: center;'>Herramientas y Tecnologías</h4>
+            Para la realización de este proyecto, hemos empleado una variedad de herramientas tecnológicas adquiridas durante nuestro curso, incluyendo Python, Pandas, Numpy, Plotly, Matplotlib y Keras. Estas herramientas han sido fundamentales para procesar y analizar grandes volúmenes de datos de manera eficiente, así como para la visualización clara e interactiva de los resultados, facilitando el entendimiento de las tendencias y patrones detectados.
+        </div>
+        <div style='text-align: justify;'>
+            Mediante el uso de Streamlit, hemos logrado presentar nuestros resultados de manera dinámica y accesible, proporcionando a los usuarios la posibilidad de interactuar con los datos y explorar en profundidad las diversas facetas de nuestro análisis. Este proyecto refleja no solo el aprendizaje alcanzado en Hack a Boss, sino también nuestra determinación por aplicar la ciencia de datos en la solución de problemas prácticos del mundo real. Confiamos en que los insights y visualizaciones que ofrecemos sean tanto informativos como de utilidad práctica para los interesados.
+        </div>
+    """, unsafe_allow_html=True)
     
