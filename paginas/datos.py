aee1985cd4ec12bde2f5fe12bbcad2ecdf5f4acb
kelvincabezas <ruben.cabezas@outlook.com>
2024-09-17 21:32:02 -0500

    ajustes

--- paginas/datos.py
+++ paginas/datos.py
@@ -18,4 +18,9 @@ def display():
     st.write("### Vista previa de los datos")
     st.write(df.head())
  
- 
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.header('Uso de los datos')
+
+    st.markdown("""
+    Esta importante recopilación de datos no solo facilita una variedad de análisis y visualizaciones, sino que también nos ayuda a comprender mejor sobre las dinámicas existentes en la aviación. Con estos datos, podemos explorar tendencias en el tráfico aéreo, evaluar la puntualidad de las aerolíneas, y identificar los aeropuertos más activos. Esta información es esencial no solo para investigadores y analistas sino también para entusiastas de la aviación que buscan entender mejor los patrones y comportamientos del sector. Además, permite a las partes interesadas en la industria tomar decisiones más informadas basadas en tendencias históricas y actuales.
+    """)
