255118f2fcadd41524abf636bacefcb09d1abf4d
kelvincabezas <ruben.cabezas@outlook.com>
2024-09-17 21:52:37 -0500

    rrrrr

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -27,7 +27,7 @@ def display():
     """, unsafe_allow_html=True)
     
     st.markdown("<h3 style='text-align: center;'>Objetivos</h3><br>", unsafe_allow_html=True)
-    colizq, colder = st.columns(4)
+    colizq, colder = st.columns(3)
     with colizq:
         
         st.markdown("""
