77c18d43d998627e73463072e99cfad19330e724
kelvincabezas <ruben.cabezas@outlook.com>
2024-09-17 21:55:23 -0500

    ajustes

--- paginas/modelo_predictivo.py
+++ paginas/modelo_predictivo.py
@@ -18,7 +18,6 @@ def display():
     st.markdown("---")
     if 'subpagina_eda' not in st.session_state:
         st.markdown("<h4 style='text-align: center;'>En esta sección de nuestro proyecto, te ofrecemos la oportunidad de explorar un análisis detallado de los datos a través de diversos gráficos y visualizaciones. Este espacio está diseñado para que puedas entender mejor y analizar de forma intuitiva la información que hemos recopilado.</h4>", unsafe_allow_html=True)
-        st.image('resources/EDA.jpg')
 
     # Cargar los datos
     df = pd.read_excel('data/data.xlsx')
