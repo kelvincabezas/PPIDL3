8d1334399112bfa8ce27a70ccff557b21dd71d0d
kelvincabezas <ruben.cabezas@outlook.com>
2024-09-17 21:11:20 -0500

    ajustes

--- modules/config_page.py
+++ modules/config_page.py
@@ -4,6 +4,6 @@ import streamlit as st
 def set_global_page_config():
     st.set_page_config(
         page_title="Modelos Predictivos en la caleta COISHCO - 2024",
-        page_icon="✈️",
+        page_icon="🚢",
         layout="wide"
     )
